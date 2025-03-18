776685db6492c112cd90882e89889709a92e23c8
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2025-03-18 13:41:54 +0100

    Updates

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -1,150 +1,150 @@
 const data = [
-    {
-      id: 1,
-      title: "The Lord of the Rings",
-      publicationDate: "1954-07-29",
-      author: "J. R. R. Tolkien",
-      genres: [
-        "fantasy",
-        "high-fantasy",
-        "adventure",
-        "fiction",
-        "novels",
-        "literature",
-      ],
-      hasMovieAdaptation: true,
-      pages: 1216,
-      translations: {
-        spanish: "El señor de los anillos",
-        chinese: "魔戒",
-        french: "Le Seigneur des anneaux",
+  {
+    id: 1,
+    title: "The Lord of the Rings",
+    publicationDate: "1954-07-29",
+    author: "J. R. R. Tolkien",
+    genres: [
+      "fantasy",
+      "high-fantasy",
+      "adventure",
+      "fiction",
+      "novels",
+      "literature",
+    ],
+    hasMovieAdaptation: true,
+    pages: 1216,
+    translations: {
+      spanish: "El señor de los anillos",
+      chinese: "魔戒",
+      french: "Le Seigneur des anneaux",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.52,
+        ratingsCount: 630994,
+        reviewsCount: 13417,
       },
-      reviews: {
-        goodreads: {
-          rating: 4.52,
-          ratingsCount: 630994,
-          reviewsCount: 13417,
-        },
-        librarything: {
-          rating: 4.53,
-          ratingsCount: 47166,
-          reviewsCount: 452,
-        },
+      librarything: {
+        rating: 4.53,
+        ratingsCount: 47166,
+        reviewsCount: 452,
       },
     },
-    {
-      id: 2,
-      title: "The Cyberiad",
-      publicationDate: "1965-01-01",
-      author: "Stanislaw Lem",
-      genres: [
-        "science fiction",
-        "humor",
-        "speculative fiction",
-        "short stories",
-        "fantasy",
-      ],
-      hasMovieAdaptation: false,
-      pages: 295,
-      translations: {},
-      reviews: {
-        goodreads: {
-          rating: 4.16,
-          ratingsCount: 11663,
-          reviewsCount: 812,
-        },
-        librarything: {
-          rating: 4.13,
-          ratingsCount: 2434,
-          reviewsCount: 0,
-        },
+  },
+  {
+    id: 2,
+    title: "The Cyberiad",
+    publicationDate: "1965-01-01",
+    author: "Stanislaw Lem",
+    genres: [
+      "science fiction",
+      "humor",
+      "speculative fiction",
+      "short stories",
+      "fantasy",
+    ],
+    hasMovieAdaptation: false,
+    pages: 295,
+    translations: {},
+    reviews: {
+      goodreads: {
+        rating: 4.16,
+        ratingsCount: 11663,
+        reviewsCount: 812,
+      },
+      librarything: {
+        rating: 4.13,
+        ratingsCount: 2434,
+        reviewsCount: 0,
       },
     },
-    {
-      id: 3,
-      title: "Dune",
-      publicationDate: "1965-01-01",
-      author: "Frank Herbert",
-      genres: ["science fiction", "novel", "adventure"],
-      hasMovieAdaptation: true,
-      pages: 658,
-      translations: {
-        spanish: "",
-      },
-      reviews: {
-        goodreads: {
-          rating: 4.25,
-          ratingsCount: 1142893,
-          reviewsCount: 49701,
-        },
+  },
+  {
+    id: 3,
+    title: "Dune",
+    publicationDate: "1965-01-01",
+    author: "Frank Herbert",
+    genres: ["science fiction", "novel", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 658,
+    translations: {
+      spanish: "",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.25,
+        ratingsCount: 1142893,
+        reviewsCount: 49701,
       },
     },
-    {
-      id: 4,
-      title: "Harry Potter and the Philosopher's Stone",
-      publicationDate: "1997-06-26",
-      author: "J. K. Rowling",
-      genres: ["fantasy", "adventure"],
-      hasMovieAdaptation: true,
-      pages: 223,
-      translations: {
-        spanish: "Harry Potter y la piedra filosofal",
-        korean: "해리 포터와 마법사의 돌",
-        bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
-        portuguese: "Harry Potter e a Pedra Filosofal",
+  },
+  {
+    id: 4,
+    title: "Harry Potter and the Philosopher's Stone",
+    publicationDate: "1997-06-26",
+    author: "J. K. Rowling",
+    genres: ["fantasy", "adventure"],
+    hasMovieAdaptation: true,
+    pages: 223,
+    translations: {
+      spanish: "Harry Potter y la piedra filosofal",
+      korean: "해리 포터와 마법사의 돌",
+      bengali: "হ্যারি পটার এন্ড দ্য ফিলোসফার্স স্টোন",
+      portuguese: "Harry Potter e a Pedra Filosofal",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.47,
+        ratingsCount: 8910059,
+        reviewsCount: 140625,
       },
-      reviews: {
-        goodreads: {
-          rating: 4.47,
-          ratingsCount: 8910059,
-          reviewsCount: 140625,
-        },
-        librarything: {
-          rating: 4.29,
-          ratingsCount: 120941,
-          reviewsCount: 1960,
-        },
+      librarything: {
+        rating: 4.29,
+        ratingsCount: 120941,
+        reviewsCount: 1960,
       },
     },
-    {
-      id: 5,
-      title: "A Game of Thrones",
-      publicationDate: "1996-08-01",
-      author: "George R. R. Martin",
-      genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
-      hasMovieAdaptation: true,
-      pages: 835,
-      translations: {
-        korean: "왕좌의 게임",
-        polish: "Gra o tron",
-        portuguese: "A Guerra dos Tronos",
-        spanish: "Juego de tronos",
+  },
+  {
+    id: 5,
+    title: "A Game of Thrones",
+    publicationDate: "1996-08-01",
+    author: "George R. R. Martin",
+    genres: ["fantasy", "high-fantasy", "novel", "fantasy fiction"],
+    hasMovieAdaptation: true,
+    pages: 835,
+    translations: {
+      korean: "왕좌의 게임",
+      polish: "Gra o tron",
+      portuguese: "A Guerra dos Tronos",
+      spanish: "Juego de tronos",
+    },
+    reviews: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 2295233,
+        reviewsCount: 59058,
       },
-      reviews: {
-        goodreads: {
-          rating: 4.44,
-          ratingsCount: 2295233,
-          reviewsCount: 59058,
-        },
-        librarything: {
-          rating: 4.36,
-          ratingsCount: 38358,
-          reviewsCount: 1095,
-        },
+      librarything: {
+        rating: 4.36,
+        ratingsCount: 38358,
+        reviewsCount: 1095,
       },
     },
-  ];
-  
-  function getBooks() {
-    return data;
-  }
-  
-  function getBook(id) {
-    return data.find((d) => d.id === id);
-  }
+  },
+];
 
-  // Desctructuring
-  /* Commented out to work with Array map method
+function getBooks() {
+  return data;
+}
+
+function getBook(id) {
+  return data.find((d) => d.id === id);
+}
+
+// Desctructuring
+/* Commented out to work with Array map method
   const books = getBooks();
   const book = getBook(1);
 
@@ -211,18 +211,17 @@ function getTotalReviewCount(book){
 console.log(getTotalReviewCount(book)); // 139869
 */
 
-
 // Working with the Array map method - creates a new array with the results of calling a provided function on every element in the calling array
 // map method does not mutate the original array, but returns a new returs a new array with the modified elements
 const books = getBooks();
 
-const x = [1,2,3,4,5].map((el) => el * 2); // [2,4,6,8,10]
-console.log(x); 
+const x = [1, 2, 3, 4, 5].map((el) => el * 2); // [2,4,6,8,10]
+console.log(x);
 
 const titles = books.map((book) => book.title);
 console.log(titles);
 
-function getTotalReviewCount(book){
+function getTotalReviewCount(book) {
   const goodreadsCount = book.reviews?.goodreads?.reviewsCount;
   const librarythingCount = book.reviews?.librarything?.reviewsCount ?? 0; // If the property reviewsCount does not exist add 0 to the total
   return goodreadsCount + librarythingCount;
@@ -237,11 +236,14 @@ const essentialData = books.map((book) => ({
 essentialData;
 
 // Working with the Array filter method - creates a new array with the elements that pass the test
-const longBooks = books.filter((book) => book.pages > 800).filter((book) => book.hasMovieAdaptation);
-longBooks; 
+const longBooks = books
+  .filter((book) => book.pages > 800)
+  .filter((book) => book.hasMovieAdaptation);
+longBooks;
 
-const adventureBooks = books.filter((book) => book.genres.includes("adventure"))
-.map((book) => book.title);
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
 adventureBooks;
 
 // Working with the Array reduce method - executes a reducer function on each element of the array, resulting in a single output value
